1c0698b2e6bda01d642f7b5435c51e594a4b114e
Davide Conti <davide.conti@translated.com>
2025-05-21 10:23:37 +0200

    Fix missing DocumentStatus export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export {version} from "./sdk-version";
 export {Credentials} from "./credentials";
 export {LaraError, TimeoutError, LaraApiError} from "./errors";
 export {Translator, TranslatorOptions, Memories, MemoryImportCallback, TranslateOptions, Documents, DocumentTranslateOptions} from "./translator/translator";
-export {Memory, MemoryImport, TextBlock, TextResult, Document, DocumentUploadOptions, DocumentDownloadOptions} from "./translator/models";
+export {Memory, MemoryImport, TextBlock, TextResult, Document, DocumentStatus, DocumentUploadOptions, DocumentDownloadOptions} from "./translator/models";
 export {MultiPartFile} from "./net/client";
